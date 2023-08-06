58461a7911949d6c768751af59789783ec7c9727
AurelaFetaj <AurelaFetaj@DESKTOP-EICTVEK>
2023-08-06 10:43:33 +0200

    add first orchestration test

---
+++ NavigationModule.Journeys.Tests.Unit/Services/Orchestrations/JourneyOrchestrationServiceTests.Logic.cs
@@ -0,0 +1,41 @@
+﻿using NavigationModule.Journeys.Models.DTOs.Journeys;
+using NavigationModule.Journeys.Models.Entities.Journeys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationModule.Journeys.Tests.Unit.Services.Orchestrations
+{
+    public partial class JourneyOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddJourneyAndAchievementAsync()
+        {
+            //arrange
+            JourneyRequest inputRequest = CreateRandomObject<JourneyRequest>();
+            Journey expectedResponse = CreateRandomObject<Journey>();
+
+            this.journeyProcessingServiceMock
+                .Setup(service => service.AddJourneyAsync(inputRequest))
+                .ReturnsAsync(expectedResponse);
+
+            //act
+            var actualResult =
+                await this.journeyOrchestrationService.AddJourneyAsync(inputRequest);
+
+            //assert
+            actualResult.Should().BeEquivalentTo(expectedResponse);
+
+            this.journeyProcessingServiceMock.Verify(
+                service => service.AddJourneyAsync(inputRequest), Times.Once);
+
+            this.achievementProcessingServiceMock.Verify(
+                service => service.UpsertAchievementAsync(inputRequest), Times.Once);
+
+            this.journeyProcessingServiceMock.VerifyNoOtherCalls();
+            this.achievementProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ NavigationModule.Journeys.Tests.Unit/Services/Orchestrations/JourneyOrchestrationServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using NavigationModule.Journeys.Services.Orchestrations.Journeys;
+using NavigationModule.Journeys.Services.Processings.Achievements;
+using NavigationModule.Journeys.Services.Processings.Journeys;
+
+namespace NavigationModule.Journeys.Tests.Unit.Services.Orchestrations
+{
+    public partial class JourneyOrchestrationServiceTests
+    {
+        private readonly Mock<IAchievementProcessingService> achievementProcessingServiceMock;
+        private readonly Mock<IJourneyProcessingService> journeyProcessingServiceMock;
+        private readonly IJourneyOrchestrationService journeyOrchestrationService;
+
+        public JourneyOrchestrationServiceTests()
+        {
+            this.achievementProcessingServiceMock = new Mock<IAchievementProcessingService>();
+            this.journeyProcessingServiceMock = new Mock<IJourneyProcessingService>();
+
+            this.journeyOrchestrationService = new JourneyOrchestrationService(
+                achievementProcessingService: this.achievementProcessingServiceMock.Object,
+                journeyProcessingService: this.journeyProcessingServiceMock.Object);
+        }
+
+        private static T CreateRandomObject<T>() where T : class
+            => CreateObjectFiller<T>().Create();
+
+        private static Filler<T> CreateObjectFiller<T>()
+            where T : class
+        {
+            var filler = new Filler<T>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(DateTimeOffset.UtcNow);
+
+            return filler;
+        }
+    }
+}
